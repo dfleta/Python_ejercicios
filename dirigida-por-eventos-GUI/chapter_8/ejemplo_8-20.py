03716f358282558d5757c861ec887bf04e1ad38e
David <gelpiorama@gmail.com>
2015-05-27 20:39:33 +0200

    variables tkinter, checkbutton y radiobutton

--- dirigida-por-eventos-GUI/chapter_8/ejemplo_8-20.py
+++ dirigida-por-eventos-GUI/chapter_8/ejemplo_8-20.py
@@ -38,3 +38,28 @@ if __name__ == '__main__':
     Quitter(root).pack(side=RIGHT)
     root.bind('<Return>', (lambda event: fetch(vars)))
     root.mainloop()
+
+'''
+http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/control-variables.html 
+
+A Tkinter control variable is a special object that acts like a regular Python variable 
+in that it is a container for a value, such as a number or string.
+
+One special quality of a control variable is that it can be shared by a number of different widgets, 
+and the control variable can remember all the widgets that are currently sharing it. 
+
+This means, in particular, that if your program stores a value v into a control variable c with its c.set(v) 
+method, any widgets that are linked to that control variable are automatically updated on the screen.
+
+Checkbuttons use a control variable to hold the current state of the checkbutton (on or off) => ejemplo 8-22
+
+A single control variable is shared by a group of radiobuttons 
+and can be used to tell which one of them is currently set. => ejemplo 8-25
+
+Control variables hold text string for several applications. 
+Normally the text displayed in an Entry widget is linked to a control variable => este ejemplo 8-20
+
+    v = tk.DoubleVar()   # Holds a float; default value 0.0
+    v = tk.IntVar()      # Holds an int; default value 0
+    v = tk.StringVar()   # Holds a string; default value ''
+'''
--- dirigida-por-eventos-GUI/chapter_8/ejemplo_8-22.py
+++ dirigida-por-eventos-GUI/chapter_8/ejemplo_8-22.py
@@ -4,30 +4,52 @@ from tkinter import *             # get base widget set
 from dialogTable import demos     # get canned dialogs
 from quitter import Quitter       # attach a quitter object to "me"
 
+
 class Demo(Frame):
+
     def __init__(self, parent=None, **options):
+
         Frame.__init__(self, parent, **options)
+
         self.pack()
         self.tools()
+
         Label(self, text="Check demos").pack()
-        self.vars = []
+
+        self.vars = []  # coleccion de variables tkinter
+
         for key in demos:
             var = IntVar()
             Checkbutton(self,
                         text=key,
-                        variable=var,
+                        variable=var,   # asociamos la IntVar() a variable => es un 0 o 1
                         command=demos[key]).pack(side=LEFT)
             self.vars.append(var)
 
     def report(self):
         for var in self.vars:
             print(var.get(), end=' ')   # current toggle settings: 1 or 0
+            # muestra el valor de todas las variables tkinter definidas por el checkbutton
         print()
 
     def tools(self):
         frm = Frame(self)
         frm.pack(side=RIGHT)
         Button(frm, text='State', command=self.report).pack(fill=X)
+        # al hacer clic sobre el botón State, se ejecuta el manejador report => 
+        # muestra el valor de todas las variables tkinter definidas por el checkbutton
         Quitter(frm).pack(fill=X)
 
-if __name__ == '__main__': Demo().mainloop()
+if __name__ == '__main__': 
+
+        Demo().mainloop()
+
+'''
+The indicator is the part of the checkbutton that shows its state, 
+and the label is the text that appears beside it:
+ The graphic above shows how checkbuttons look in the off (0) and on (1) state
+
+You will need to create a control variable, an instance of the IntVar class, 
+so your program can query and set the state of the checkbutton. 
+See Section 52, “Control variables: the values behind the widgets”, below.
+'''
