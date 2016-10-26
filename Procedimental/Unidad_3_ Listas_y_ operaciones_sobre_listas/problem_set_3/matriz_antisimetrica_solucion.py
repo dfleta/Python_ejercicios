7b06fbe993b13ef8324dbb1e9f9209ef506b244f
David <gelpiorama@gmail.com>
2016-10-26 12:54:59 +0200

    manejo de errores ejercicios matrices

--- Procedimental/Unidad_3_ Listas_y_ operaciones_sobre_listas/problem_set_3/matriz_antisimetrica_solucion.py
+++ Procedimental/Unidad_3_ Listas_y_ operaciones_sobre_listas/problem_set_3/matriz_antisimetrica_solucion.py
@@ -1,65 +1,69 @@
 # By Dimitris_GR from forums
-# Modify Problem Set 31's (Optional) Symmetric Square to return True 
-# if the given square is esAntisimetrica and False otherwise. 
-# An nxn square is called esAntisimetrica if A[i][j]=-A[j][i] 
+# Modify Problem Set 31's (Optional) Symmetric Square to return True
+# if the given square is esAntisimetrica and False otherwise.
+# An nxn square is called esAntisimetrica if A[i][j]=-A[j][i]
 # for each i=0,1,...,n-1 and for each j=0,1,...,n-1.
 
+
 def esAntisimetrica(matriz):
-    
-    assert len(matriz) == len(matriz[0])
+
+    # precondicion: manejo de errores.
+    # The public methods assume the data is unsafe,
+    # and they are responsible for checking the data (and sanitizing it).
+
+    if len(matriz) != len(matriz[0]):
+        return False
 
     i = 0
     j = 0
 
     while i <= len(matriz) - 1:
 
-    	while j <= len(matriz[i]) - 1:
+        while j <= len(matriz[i]) - 1:
 
-    		if matriz[i][j] != - matriz[j][i]:
-    			return False
+            if matriz[i][j] != - matriz[j][i]:
 
-    		j += 1
+                return False
 
-    	i += 1
+            j += 1
+
+        i += 1
 
     return True
 
 
-
-
-
 # Test Cases:
 
-print esAntisimetrica([[0, 1, 2], 
-                     [-1, 0, 3], 
-                     [-2, -3, 0]])   
-#>>> True
+print(esAntisimetrica([[0, 1, 2],
+                       [-1, 0, 3],
+                       [-2, -3, 0]]))
+# >>> True
 
-print esAntisimetrica([[0, 0, 0],
-                     [0, 0, 0],
-                     [0, 0, 0]])
-#>>> True
+print(esAntisimetrica([[0, 0, 0],
+                       [0, 0, 0],
+                       [0, 0, 0]]))
+# >>> True
 
-print esAntisimetrica([[0, 1, 2], 
-                     [-1, 0, -2], 
-                     [2, 2,  3]])
-#>>> False
+print(esAntisimetrica([[0, 1, 2],
+                       [-1, 0, -2],
+                       [2, 2, 3]]))
+# >>> False
 
-print esAntisimetrica([[1, 2, 5],
-                     [0, 1, -9],
-                     [0, 0, 1]])
-#>>> False
+print(esAntisimetrica([[1, 2, 5],
+                       [0, 1, -9],
+                       [0, 0, 1]]))
+# >>> False
 
 # casos test que no satisfacen la precondicion de que la matriz sea cuadrada:
 
-matriz4 = [[1,0,0,0],
-           [0,1,1,0],
-           [0,0,0,1]]
+matriz4 = [[1, 0, 0, 0],
+           [0, 1, 1, 0],
+           [0, 0, 0, 1]]
 
-print esAntisimetrica(matriz4)
-#>>>False
+print(esAntisimetrica(matriz4))
+# >>>False
 
-matriz5 = [[1,0,0,0,0,0,0,0,0]]
+matriz5 = [[1, 0, 0, 0, 0, 0, 0, 0, 0]]
 
-print esAntisimetrica(matriz5)
-#>>>False 
+print(esAntisimetrica(matriz5))
+# >>>False
