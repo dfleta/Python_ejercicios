40cd83ab792950f9f48e25e0a6c4c1413fc2530a
David <gelpiorama@gmail.com>
2016-10-27 20:39:41 +0200

    sudoku resuelto sin aserciones

--- Procedimental/Unidad_3_ Listas_y_ operaciones_sobre_listas/problem_set_3/08-Sudoku_resuelto.py
+++ Procedimental/Unidad_3_ Listas_y_ operaciones_sobre_listas/problem_set_3/08-Sudoku_resuelto.py
@@ -30,74 +30,81 @@
 # You may assume the the input is square and contains at
 # least one row and column.
 
-correct = [[1,2,3],
-           [2,3,1],
-           [3,1,2]]
+correcto = [[1, 2, 3],
+            [2, 3, 1],
+            [3, 1, 2]]
 
-incorrect = [[1,2,3,4],
-             [2,3,1,3],
-             [3,1,2,3],
-             [4,4,4,2]]
+incorrecto1 = [[1, 2, 3, 4],
+               [2, 3, 1, 3],
+               [3, 1, 2, 3],
+               [4, 4, 4, 4]]
 
-incorrect2 = [[1,2,3,4],
-              [2,3,1,2],
-              [4,1,2,3],
-              [2,3,1,4]]
+incorrecto2 = [[1, 2, 3, 4],
+               [2, 3, 1, 4],
+               [4, 1, 2, 3],
+               [3, 4, 1, 2]]
 
-incorrect3 = [[1,2,3,4,5],
-              [2,3,1,5,6],
-              [4,5,2,1,3],
-              [3,4,5,2,1],
-              [5,6,4,3,2]]
+incorrecto3 = [[1, 2, 3, 4, 5],
+               [2, 3, 1, 5, 6],
+               [4, 5, 2, 1, 3],
+               [3, 4, 5, 2, 1],
+               [5, 6, 4, 3, 2]]
 
-incorrect4 = [['a','b','c'],
-              ['b','c','a'],
-              ['c','a','b']]
+incorrecto4 = [['a', 'b', 'c'],
+               ['b', 'c', 'a'],
+               ['c', 'a', 'b']]
 
-incorrect5 = [ [1, 1.5],
+incorrecto5 = [[1, 1.5],
                [1.5, 1]]
 
-irregular = [[1,2,3],
-             [2,3,1]]            
+incorrecto6 = [[1, 0, 0, 0],
+               [0, 1, 0],
+               [0, 0, 0, 1]]
+
 
 def esCuadrado(sudoku):
 
-  numeroFilas = len(sudoku)
+    numeroFilas = len(sudoku)
 
-  for fila in sudoku:
+    for fila in sudoku:
 
-    if len(fila) != numeroFilas:
-        return False      
+        if len(fila) != numeroFilas:
+            return False
 
-  return True
+    return True
 
+
+def sonNumerosEnteros(sudoku):
+
+    for fila in sudoku:
+
+        for numero in fila:
+
+            if not isinstance(numero, int):
+                return False
+
+    return True
+
+
+def numerosEnRango(sudoku):
+
+    numerosValidos = range(1, len(sudoku) + 1)
+
+    for fila in sudoku:
+
+        for numero in fila:
+
+            if numero not in numerosValidos:
+                return False
+
+    return True
 
 
 def checkNumerosValidos(sudoku):
 
     # precondiciones
 
-    for fila in sudoku:
-      for numero in fila:
-
-          try:
-            assert isinstance(numero, int)
-
-          except AssertionError:
-                  return False
-
-
-    numerosValidos = range(1, len(sudoku) + 1)
-
-    for fila in sudoku:
-
-      for numero in fila:
-
-          if numero not in numerosValidos:
-            return False
-
-    return True
-
+    return sonNumerosEnteros(sudoku) and numerosEnRango(sudoku)
 
 
 def checkFilas(sudoku):
@@ -107,21 +114,22 @@ def checkFilas(sudoku):
         posicionNumero = 0
 
         for numero in fila:
-            # Averiguo si el numero se encuentra en el resto de la fila /lista (siguiente posicion hasta la ultima)
-            if numero in fila[posicionNumero+1:]:
+                # Averiguo si el numero se encuentra en el resto de la fila
+                # El resto de la fila es una lista formada
+                # desde la siguiente posicion del numero actual hasta la ultima
+            if numero in fila[posicionNumero + 1:]:
                 return False
             else:
+                # incremento la posicion desde la que buscar
                 posicionNumero += 1
 
     return True
 
 
-
-
 def checkColumnas(sudoku):
 
     primeraFila = sudoku[0]
- 
+
     numeroDeFilas = len(primeraFila) - 1
 
     for numero in primeraFila:
@@ -129,71 +137,73 @@ def checkColumnas(sudoku):
         indexFilaActual = 0
 
         # Buscamos cada elemento de la primera fila en el resto de filas:
-        # Si el indice que ocupa en las otras filas es igual al que ocupa en la primera: mal <=>
-        # <=> no puede haber dos numeros iguales en la misma columna.
+        # Si el indice que ocupa en las otras filas es igual al que ocupa
+        # en la primera: mal <=> no puede haber dos numeros iguales en la misma columna.
+
         while indexFilaActual < numeroDeFilas:
 
             indexFilaSiguiente = indexFilaActual + 1
 
             try:
-                # El elemento puede no existir en la fila siguiente: 
+                # El elemento puede no existir en la fila siguiente:
                 # index devuelve excepcion: ValueError = mensaje "x is not in list"
-                # Imposible alcanzar esta excepcion si checkColumnas se ejecuta despues de checkFilas
+                # Imposible alcanzar esta excepcion si checkColumnas se ejecuta
+                # despues de checkFilas
                 posicionNumeroFilaSiguiente = sudoku[indexFilaSiguiente].index(numero)
 
             except ValueError:
-                      # Este bloque de codigo se ejecuta si sudoku[indexFilaSiguiente].index(numero)
-                      # devuelve un error <=> si el numero no esta en la fila
-                      return False
+                # Este bloque de codigo se ejecuta si sudoku[indexFilaSiguiente].index(numero)
+                # devuelve un error <=> si el numero no esta en la fila
+                # ValueError: numero is not in list
+                return False
 
             else:
                 # Este bloque de codigo se ejecuta si la sentencia sudoku[indexFilaSiguiente].index(numero)
                 # ha ido bien <=> el numero esta en la fila
                 if posicionNumeroFilaSiguiente == primeraFila.index(numero):
-                      return False
+                    return False
                 else:
-                      indexFilaActual += 1
+                    indexFilaActual += 1
 
     return True
- 
 
 
 def check_sudoku(sudoku):
-    
+
     return esCuadrado(sudoku) and checkNumerosValidos(sudoku) and checkFilas(sudoku) and checkColumnas(sudoku)
 
 
 # Casos test:
 
 # print checkFilas(correct)
-#>>> True
+# >>> True
 # print checkFilas(incorrect)
-#>>> False
+# >>> False
 
 # print checkColumnas(correct)
-#>>> True
+# >>> True
 # print checkColumnas(incorrect2)
-#>>> False
+# >>> False
 
-print check_sudoku(irregular)
-#>>> False
-    
-print check_sudoku(incorrect)
-#>>> False
+# Casos test
 
-print check_sudoku(correct)
-#>>> True
+print(check_sudoku(incorrecto1))
+# >>> False
 
-print check_sudoku(incorrect2)
-#>>> False
+print(check_sudoku(correcto))
+# >>> True
 
-print check_sudoku(incorrect3)
-#>>> False
+print(check_sudoku(incorrecto2))
+# >>> False
 
-print check_sudoku(incorrect4)
-#>>> False
+print(check_sudoku(incorrecto3))
+# >>> False
 
-print check_sudoku(incorrect5)
-#>>> False
+print(check_sudoku(incorrecto4))
+# >>> False
 
+print(check_sudoku(incorrecto5))
+# >>> False
 
+print(check_sudoku(incorrecto6))
+# >>> False
