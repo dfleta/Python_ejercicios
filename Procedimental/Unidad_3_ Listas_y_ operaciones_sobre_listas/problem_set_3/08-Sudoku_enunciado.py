187986a9f3510dde35daa2a1f91d34f7653a39cf
David <gelpiorama@gmail.com>
2016-10-27 16:15:16 +0200

    sudoku enunciado formateado a PEP8 y Py3

--- Procedimental/Unidad_3_ Listas_y_ operaciones_sobre_listas/problem_set_3/08-Sudoku_enunciado.py
+++ Procedimental/Unidad_3_ Listas_y_ operaciones_sobre_listas/problem_set_3/08-Sudoku_enunciado.py
@@ -30,63 +30,61 @@
 # You may assume the the input is square and contains at
 # least one row and column.
 
-correct = [[1,2,3],
-           [2,3,1],
-           [3,1,2]]
+correcto = [[1, 2, 3],
+            [2, 3, 1],
+            [3, 1, 2]]
 
-incorrect = [[1,2,3,4],
-             [2,3,1,3],
-             [3,1,2,3],
-             [4,4,4,4]]
+incorrecto1 = [[1, 2, 3, 4],
+               [2, 3, 1, 3],
+               [3, 1, 2, 3],
+               [4, 4, 4, 4]]
 
-incorrect2 = [[1,2,3,4],
-             [2,3,1,4],
-             [4,1,2,3],
-             [3,4,1,2]]
+incorrecto2 = [[1, 2, 3, 4],
+               [2, 3, 1, 4],
+               [4, 1, 2, 3],
+               [3, 4, 1, 2]]
 
-incorrect3 = [[1,2,3,4,5],
-              [2,3,1,5,6],
-              [4,5,2,1,3],
-              [3,4,5,2,1],
-              [5,6,4,3,2]]
+incorrecto3 = [[1, 2, 3, 4, 5],
+               [2, 3, 1, 5, 6],
+               [4, 5, 2, 1, 3],
+               [3, 4, 5, 2, 1],
+               [5, 6, 4, 3, 2]]
 
-incorrect4 = [['a','b','c'],
-              ['b','c','a'],
-              ['c','a','b']]
+incorrecto4 = [['a', 'b', 'c'],
+               ['b', 'c', 'a'],
+               ['c', 'a', 'b']]
 
-incorrect5 = [ [1, 1.5],
+incorrecto5 = [[1, 1.5],
                [1.5, 1]]
-               
 
-
-
+incorrecto6 = [[1, 0, 0, 0],
+               [0, 1, 0],
+               [0, 0, 0, 1]]
 
 
 def check_sudoku(sudoku):
     # Tu código aquí
 
 
+# Casos test
 
+print(check_sudoku(incorrecto1))
+# >>> False
 
+print(check_sudoku(correcto))
+# >>> True
 
-# Casos test:   
-    
-print check_sudoku(incorrect)
-#>>> False
+print(check_sudoku(incorrecto2))
+# >>> False
 
-print check_sudoku(correct)
-#>>> True
+print(check_sudoku(incorrecto3))
+# >>> False
 
-print check_sudoku(incorrect2)
-#>>> False
+print(check_sudoku(incorrecto4))
+# >>> False
 
-print check_sudoku(incorrect3)
-#>>> False
+print(check_sudoku(incorrecto5))
+# >>> False
 
-print check_sudoku(incorrect4)
-#>>> False
-
-print check_sudoku(incorrect5)
-#>>> False
-
-
+print(check_sudoku(incorrecto6))
+# >>> False
