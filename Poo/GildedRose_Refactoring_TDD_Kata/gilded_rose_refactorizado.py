ae0332c57eecb531bd214b0bdae6ae19dd8ada92
David <gelpiorama@gmail.com>
2016-12-15 14:18:27 +0100

    herencia refactorizada respetando principio OCP

--- Poo/GildedRose_Refactoring_TDD_Kata/gilded_rose_refactorizado.py
+++ Poo/GildedRose_Refactoring_TDD_Kata/gilded_rose_refactorizado.py
@@ -28,85 +28,104 @@ class Item:
 class Interfaz():
 
     def update_quality(self):
+        # Comportamiento a implementar en las subclases
         pass
 
 
 class NormalItem(Item, Interfaz):
+    # Herencia múltiple
 
     def __init__(self, name, sell_in, quality):
         Item.__init__(self, name, sell_in, quality)
 
+    def setSell_in(self):
+        self.sell_in = self.sell_in - 1
+
     def setQuality(self, valor):
-        if self.quality + valor >= 0:
+        if self.quality + valor > 50:
+            self.quality = 50
+        elif self.quality + valor >= 0:
             self.quality = self.quality + valor
         else:
             self.quality = 0
-        assert self.quality >= 0, "quality de %s fuera de rango" % self.__class__.__name__
+        assert 0 <= self.quality <= 50, "quality de %s fuera de rango" % self.__class__.__name__
 
+    # Override metodo update_quality de la interfaz
     def update_quality(self):
         if self.sell_in > 0:
             self.setQuality(-1)
         else:
             self.setQuality(-2)
-        self.sell_in = self.sell_in - 1
+        self.setSell_in()
 
 
-class ConjuredItem(NormalItem, Interfaz):
+class ConjuredItem(NormalItem):
 
     def __init__(self, name, sell_in, quality):
         NormalItem.__init__(self, name, sell_in, quality)
 
+    # Override metodo update_quality de la Interfaz
+    # Heredado a traves de NormalItem
     def update_quality(self):
         if self.sell_in >= 0:
             self.setQuality(-2)
         else:
             self.setQuality(-4)
-        self.sell_in = self.sell_in - 1
+        self.setSell_in()
 
 
-class AgedBrie(Item):
+class AgedBrie(NormalItem):
 
     def __init__(self, name, sell_in, quality):
         Item.__init__(self, name, sell_in, quality)
 
+    # Override metodo setQuality de NormalItem
     def setQuality(self, valor):
+        """
         if self.quality + valor <= 50:
             self.quality = self.quality + valor
         else:
             self.quality = 50
-        assert self.quality <= 50, "quality de %s fuera de rango" % self.__class__.__name__
+        """
+        NormalItem.setQuality(self, valor)
+        # assert 0 <= self.quality <= 50, "quality de %s fuera de rango" % self.__class__.__name__
 
+    # Override metodo update_quality de la Interfaz
     def update_quality(self):
         if self.sell_in > 0:
             self.setQuality(1)
         else:
             self.setQuality(2)
-        self.sell_in = self.sell_in - 1
+        self.setSell_in()
 
 
-class Sulfuras(Item, Interfaz):
+class Sulfuras(NormalItem):
 
     def __init__(self, name, sell_in, quality):
         Item.__init__(self, name, sell_in, quality)
 
+    # Override metodo update_quality de la interfaz
     def update_quality(self):
-        assert self.quality != 80, "quality de %s distinta de 80" % self.__class__.__name__
+        assert self.quality == 80, "quality de %s distinta de 80" % self.__class__.__name__
         pass
-        # falla: self.quality = self.quality + 1
 
 
-class Backstage(Item, Interfaz):
+class Backstage(NormalItem):
 
     def __init__(self, name, sell_in, quality):
         Item.__init__(self, name, sell_in, quality)
 
     def setQuality(self, valor):
+        """
         if self.quality + valor <= 50:
             self.quality = self.quality + valor
         else:
             self.quality = 50
-        assert self.quality <= 50, "quality de %s fuera de rango" % self.__class__.__name__
+        """
+        NormalItem.setQuality(self, valor)
+        assert 0 <= self.quality <= 50, "quality de %s fuera de rango" % self.__class__.__name__
 
+    # Override metodo update_quality de la interfaz
     def update_quality(self):
         if self.sell_in > 10:
             self.setQuality(1)
@@ -115,5 +134,6 @@ class Backstage(Item, Interfaz):
         elif self.sell_in > 0:
             self.setQuality(3)
         else:
+            # ocultar informacion: uf
             self.quality = 0
-        self.sell_in = self.sell_in - 1
+        self.setSell_in()
