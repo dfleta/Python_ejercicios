0defef8ea39bf99cbd36798dc6f2c9f02a2e6837
David <gelpiorama@gmail.com>
2017-01-11 13:07:47 +0100

    test metodo estatico Yatzy

--- Poo/Yatzy_Refactoring_TDD_Kata/test_yatzy_from_scratch.py
+++ Poo/Yatzy_Refactoring_TDD_Kata/test_yatzy_from_scratch.py
@@ -1,6 +1,16 @@
 import pytest
 from yatzy import Yatzy
 
+# Chance
+# The player scores the sum of all dice, no matter what they read.
+
+
+def test_chance():
+    # iterar sobre *args evita codigo cableado a 5 argumentos
+    assert 15 == Yatzy.chance(1, 2, 3, 4, 5)
+    assert 14 == Yatzy.chance(1, 1, 3, 3, 6)
+    assert 21 == Yatzy.chance(4, 5, 5, 6, 1)
+
 
 @pytest.fixture
 def inyector():
